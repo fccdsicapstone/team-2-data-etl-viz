72c1370e47fc56da8b53d904e4d7cda52b93ff7a
Zixuan Li <armstrong0220@gmail.com>
2018-11-11 00:21:59 -0500

    modify files

--- SQL_CREATE_TABLE/acs_blockgroup2_[year].sql
+++ SQL_CREATE_TABLE/acs_blockgroup2_[year].sql
@@ -27,3 +27,7 @@ CREATE OR REPLACE TABLE broadband.acs_blockgroup2_2014(
        county STRING,
        tract STRING,
        block_group STRING)
+
+CREATE OR REPLACE TABLE broadband.acs_blockgroup2_2014 AS (
+       SELECT FORMAT("%s", CONCAT(LPAD(state, 2, "0"), LPAD(county, 3, "0"), LPAD(tract, 6, "0"), block_group)) AS block_group_code, * EXCEPT(state, county, tract, block_group)
+        FROM broadband.acs_blockgroup2_2014)
